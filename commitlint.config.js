3f82970e8cec4504b259d01f5226b6a0830dbb4a
Raphael Cunha <raphael.s.cunha@gmail.com>
2021-08-18 17:55:34 -0300

    feat(project): commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [2, 'always', 72]
+    'header-max-length': [2, 'always', 72],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'wip']
+    ]
   }
 };
