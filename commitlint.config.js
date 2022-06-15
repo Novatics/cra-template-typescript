2a39bcb46d5e66a2ec2d418a880299e4fa3bf6a9
Daniel Auler <danielauler7@gmail.com>
2022-06-15 11:39:55 -0300

    adiciona documentacao sobre commits, corrige erros de lint e adiciona script para executar testes no pre-commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'wip']
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'wip', 'chore']
     ]
   }
 };
